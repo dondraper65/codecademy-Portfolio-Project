b381dc0a29a0cad6d1736e63265c48e1ed431039
Rahul Singh Raghav <officialrahulsr@gmail.com>
2021-06-25 12:31:25 +0530

    Mood horoscope function added using switch.

--- message-generator.js
+++ message-generator.js
@@ -2,3 +2,20 @@ let name = "Don Draper"
 const greeting = (name) =>{
     console.log(`Hello ${name}! Your horoscope for today will cover 3 aspects of your day. Mood, Work and Love.`);
 }
+
+const mood =  (num) => {
+    console.log("Mood Horoscope: ")
+    switch (num){
+        case 0:
+            console.log("Pleasant mood. Plan a short trip with family, if free.");
+        case 1:
+            console.log("Angry mood ThunderClouds lurking. Meditate and try to stay calm in situations.");
+        case 2:
+            console.log("Horny mood. control your dick/pussy. Don't let it affect work.");
+        default:
+            console.log("Unpredicatable mood.")
+    }
+    console.log("");
+}
+
+
